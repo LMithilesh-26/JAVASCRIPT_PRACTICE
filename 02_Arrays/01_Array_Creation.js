c2001ceaa1251dbc33c0328da44504b5ab315508
Mithilesh Lathkar <157916097+LMithilesh-26@users.noreply.github.com>
2024-02-06 10:10:13 +0000

    Array Started

---
+++ 02_Arrays/01_Array_Creation.js
@@ -0,0 +1,8 @@
+const myarr = [0, 1, 2, 3, 4, true, "hello"] // contains mix of datatypes
+
+console.log(myarr[1]);
+console.log(myarr[5]);
+
+// In JS array compy operations create shallow copies(Share same reference point).
+
+const myarr2 = new Array(1, 2, 3, 4, 5)
